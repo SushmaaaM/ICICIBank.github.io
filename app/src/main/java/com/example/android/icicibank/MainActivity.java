d696e6c60447a5dc7d46a7d5cf8e91693864906c
Sushma <sushmaamath@gmail.com>
2024-07-17 17:35:26 +0530

    package name changed in all files

--- app/src/main/java/com/example/android/icicibank/MainActivity.java
+++ app/src/main/java/com/example/android/icicibank/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.icicibank;
+package com.example.android.icicibank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.example.android.icicibank.R;
 
 public class MainActivity extends AppCompatActivity {
     private WebView ourBrow;
