cf49474efe7e84d0686295e067cdcba2a818c91c
Sushma <sushmaamath@gmail.com>
2024-07-17 15:29:33 +0530

    everything is undone

--- app/src/main/java/com/example/icicibank/MainActivity.java
+++ app/src/main/java/com/example/icicibank/MainActivity.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView ourBrow;
@@ -19,9 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ourBrow = findViewById(R.id.webView);
+        ourBrow = (WebView) findViewById(R.id.webView);
         ourBrow.getSettings().setJavaScriptEnabled(true);
-        ourBrow.getSettings().setDomStorageEnabled(true);
         ourBrow.setWebViewClient(new MyBrowser());
         ourBrow.loadUrl("https://www.icicibank.com/");
 
@@ -29,15 +25,9 @@ public class MainActivity extends AppCompatActivity {
         handleIntent();
     }
 
-//    @Override
-//    protected void onNewIntent(Intent intent) {
-//        super.onNewIntent(intent);
-//        setIntent(intent);
-//        handleIntent();
-//    }
-
     private void handleIntent() {
         Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
         Uri appLinkData = appLinkIntent.getData();
         if (appLinkData != null) {
             String url = appLinkData.toString();
@@ -50,11 +40,6 @@ public class MainActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
-        }
-
-        @Override
-        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-            Toast.makeText(view.getContext(), "Failed to load webpage: " , Toast.LENGTH_SHORT).show();
         }
     }
 }
